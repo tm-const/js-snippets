6d1f6f8c0516e6a7e0f0673984ba69ec148cc733
tm-const <dev@mannyquintanilla.com>
2019-10-31 15:21:15 -0400

    js UUID identifier

--- snippets.js
+++ snippets.js
@@ -4,15 +4,17 @@
 // Reference article here: https://www.w3resource.com/javascript-exercises/javascript-math-exercise-23.php
 
 function create_UUID(){
-    var dt = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = (dt + Math.random()*16)%16 | 0;
-        dt = Math.floor(dt/16);
-        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
-    });
-    return uuid;
+  var dt = new Date().getTime();
+  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = (dt + Math.random()*16)%16 | 0;
+      dt = Math.floor(dt/16);
+      return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+  });
+  return uuid;
 }
 
-console.log(create_UUID());
+const iio = create_UUID();
 
-
+document.querySelector('#tmp_button-85389').click(function() {
+	location.href = "https://jordanbelfort.clickfunnels.com/mainevent1/?cf-affiliate-id=" + iio;
+});
