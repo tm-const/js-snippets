de00b55d5afbd743177f78c4fabb7a62cf47e7a1
Manny <55524897+tm-const@users.noreply.github.com>
2020-05-25 02:13:21 -0400

    Update snippets.js

--- snippets.js
+++ snippets.js
@@ -31,6 +31,28 @@ function getUrlVars() {
     return vars;
 }
 
+// -------------------------------------------------
+// @media JS
+// -------------------------------------------------
+
+
+$(document).ready(function () {
+    $(".fa-code").click(function () {
+        if ($(window).width() <= 833) {
+            $("#sub_navs").slideToggle();
+        }
+    });
+    $("#sub_navs").click(function () {
+        $("#sub_navs").slideToggle();
+    });
+    $(window).resize(function () {
+        if ($(window).width() >= 832.5) {
+            $("#sub_navs").hide();
+        }
+    });
+});
+
+
 
 // -------------------------------------------------
 // LocalStorage in JavaScript: How to
