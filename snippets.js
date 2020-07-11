189e72e5f008b2469afca7ea597f9814dd3a28d5
Manny <55524897+tm-const@users.noreply.github.com>
2020-07-11 15:10:56 -0400

    Update snippets.js

--- snippets.js
+++ snippets.js
@@ -142,7 +142,8 @@ setInterval(AlmostCakeAT_l0gdh, 500);
 // Device Detection
 // -------------------------------------------------
 
-
+<script type="text/javascript">
+	
   var isMobile = false; //initiate as false
 // device detection
 if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
@@ -150,3 +151,4 @@ if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine
     isMobile = true;
     console.log('')
 }
+</script>
